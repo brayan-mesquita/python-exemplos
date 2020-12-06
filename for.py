d84f53b08bc62e233f2b565809ce524ab106de7a
brayan-mesquita <brayanmesquita@gmail.com>
2020-12-06 17:14:20 -0400

    inserindo novos arquivos

--- for.py
+++ for.py
@@ -1,10 +1,10 @@
 # CONTAR DE 1 A 20, PULANDO DE 4 EM 4
-for i in range(1,20,4):
+for i in range(0,100,25):
 	print(i)
 
 #SEPARANDO NUMEROS PARES DOS IMPARES
 
-for x in range(20):
+for x in range(100):
 	if x % 2 == 0:
 		print('Numero pá: ' + str(x))
 
